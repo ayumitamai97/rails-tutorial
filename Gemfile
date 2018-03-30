7d0f69e70eb5c4ed73dfe242d90688504031ca55
玉井 あゆみ <ayumi_tamai@a-143.local>
2018-03-30 17:40:54 +0900

    added test

--- Gemfile
+++ Gemfile
@@ -35,6 +35,8 @@ gem 'jbuilder', '~> 2.5'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+gem 'rails-controller-testing'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', '9.0.6', platform: :mri
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get '/signup', to: 'users#new' #hoge_pathのhogeはuri
+
   root 'static_pages#home'
-  get  '/help',    to: 'static_pages#help', as: 'helf' # as: は名前付きルート => helf_pathになる
+  get  '/help',    to: 'static_pages#help'
+  # get  '/help',    to: 'static_pages#help', as: 'helf' # as: は名前付きルート => helf_pathになる
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
 end
