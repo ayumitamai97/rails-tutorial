a41c4d76141694ab86405e291bc6aeaa45ec2b61
玉井 あゆみ <ayumi_tamai@a-143.local>
2018-04-03 10:44:55 +0900

    users show

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   # get  '/help',    to: 'static_pages#help', as: 'helf' # as: は名前付きルート => helf_pathになる
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
+  resources :users # ユーザー情報を表示するURL (/users/1) を追加するためだけのものではありません。サンプルアプリケーションにこの１行を追加すると、ユーザーのURLを生成するための多数の名前付きルート (5.3.3) と共に、RESTfulなUsersリソースで必要となるすべてのアクションが利用できるようになるのです。
 end
