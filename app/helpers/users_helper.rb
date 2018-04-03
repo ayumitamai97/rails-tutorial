e8f868088395260615675015fd1efa853621b86f
玉井 あゆみ <ayumi_tamai@a-143.local>
2018-04-03 11:31:14 +0900

    gravatar

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,14 @@
 module UsersHelper
+  # def gravatar_for(user, options = { size: size }) # erbからsizeを受け取る
+  #   gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+  #   size = options[:size]
+  #   gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+  #   image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  # end
+  # 引数で与えられたユーザーのGravatar画像を返す
+  def gravatar_for(user, size: 80) # sizeを直接渡す
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
 end
