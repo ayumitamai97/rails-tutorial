1ed8a4df4d78c1c2d559a0634fd9b8682b1ded56
玉井 あゆみ <ayumi_tamai@a-143.local>
2018-04-13 15:11:01 +0900

    とちゅう

--- test/helpers/sessions_helper_test.rb
+++ test/helpers/sessions_helper_test.rb
@@ -1,12 +1,13 @@
-require "test_helper"
+require 'test_helper'
 
 class SessionsHelperTest < ActionView::TestCase
+
   def setup
     @user = users(:michael)
     remember(@user)
   end
 
-  test "current_user returns right user even when session is nil" do
+  test "current_user returns right user when session is nil" do
     assert_equal @user, current_user
     assert is_logged_in?
   end
@@ -15,5 +16,4 @@ class SessionsHelperTest < ActionView::TestCase
     @user.update_attribute(:remember_digest, User.digest(User.new_token))
     assert_nil current_user
   end
-
 end
