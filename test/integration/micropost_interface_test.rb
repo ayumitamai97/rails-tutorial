7daf486ed3eb5f242583632c8a80f6fc7faca786
Ayumi <ayumi.tamai@pixta.co.jp>
2018-05-27 23:36:30 +0900

    画像を検証する

--- test/integration/micropost_interface_test.rb
+++ test/integration/micropost_interface_test.rb
@@ -10,6 +10,8 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     log_in_as(@user)
     get root_path
     assert_select 'div.pagination'
+    assert_select 'input[type="file"]'
+
     # 無効な送信
     assert_no_difference 'Micropost.count' do
       post microposts_path, params: { micropost: { content: "" } }
@@ -17,9 +19,11 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     assert_select 'div#error_explanation'
     # 有効な送信
     content = "This micropost really ties the room together"
+    picture = fixture_file_upload('test/fixtures/rails.png', 'image/png')
     assert_difference 'Micropost.count', 1 do
-      post microposts_path, params: { micropost: { content: content } }
+      post microposts_path, params: { micropost: { content: content, picture: picture } }
     end
+    assert assigns(:micropost).picture? # assignsメソッドを使ってください。このメソッドを使うと、投稿に成功した後にcreateアクション内のマイクロポストにアクセスするようになります。
     assert_redirected_to root_url
     follow_redirect!
     assert_match content, response.body
@@ -38,7 +42,8 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     log_in_as(@user)
     get root_path
     # 全てのmicroposts
-    assert_match "300 microposts", response.body
+    assert_match "33 microposts", response.body
+    # fixturesでuser: michaelとなっているものの数
 
     # まだマイクロポストを投稿していないユーザー
     other_user = users(:malory)
@@ -47,7 +52,7 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     assert_match "0 microposts", response.body
     other_user.microposts.create!(content: "A micropost")
     get root_path
-    assert_match "1 microposts", response.body
+    assert_match "1 micropost", response.body
   end
 
 end
